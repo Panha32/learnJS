df25eca0aad7a91688198b893df218e6fd533b9e
Panha32 <panharavuth2526@gmail.com>
2024-10-26 16:03:45 +0700

    Third commit

---
+++ conditions.js
@@ -0,0 +1,54 @@
+console.log("<<<<< Conditions >>>>>");
+
+console.log("- The if statement");
+// if(confirm("Are you Panha JS?")) {
+//     console.log("Hello Panha JS!");
+// }
+// else {
+//     console.log("Who are you?");
+// }
+
+// console.log("1" == 1);
+// console.log("1" === 1);
+
+// var myNumber = 42;
+// if(myNumber == 42) {
+//     console.log("Yes, correct!");
+// }
+
+// var foo = 1;
+// var bar = 2;
+// if(foo < bar) {
+//     console.log("foo is smaller than bar!");
+// }
+
+// var foo = 1;
+// var bar = 2;
+// var moo = 3;
+// if(foo < bar && moo > bar) {
+//     console.log("foo is smaller than bar AND moo is bigger than bar!");
+// }
+
+// var notTrue = false;
+// if(!notTrue) {
+//     console.log("not not true is true!");
+// }
+
+console.log("- The switch statement");
+
+// var rank = "Commander";
+// switch(rank) {
+//     case "Private":
+//     case "Sergeant":
+//         console.log("You are not authorized!");
+//         break;
+//     case "Commander":
+//         console.log("Hello Commander! What can I do for you today?");
+//         break;
+//     case "Captain":
+//         console.log("Hello Captain! I will do anything you wish");
+//         break;
+//     default:
+//         console.log("I do not know what your rank is.");
+//         break;
+// }
---
+++ loops.js
@@ -0,0 +1,43 @@
+console.log("<<<<< Loops >>>>>");
+
+console.log("- The for statement");
+
+// var i;
+// for(i = 0; i < 3; i = i + 1) {
+//     console.log(i);
+// }
+
+// for(var i = 0; i < 3; i++) {
+//     console.log(i);
+// }
+
+// var myArray = ["a", "b", "c"];
+// for(var i = 0; i < myArray.length; i++) {
+//     console.log("The member of myArray in index " + i + " is " + myArray[i]);
+// }
+
+console.log("- The while statement");
+
+// var i = 10;
+// while(i > 0) {
+//     console.log(i + " bottles of beer in the wall");
+//     i -= 1;
+// }
+
+console.log("- break and continue statements");
+
+// var i = 10;
+// while(i > 0) {
+//     console.log(i + " bottles of beer in the wall");
+//     i -= 1;
+//     if(i == 3) {
+//         break;
+//     }
+// }
+
+// for(var i = 0; i < 20; i++) {
+//     if(i % 2 == 0) {
+//         continue;
+//     }
+//     console.log(i + " is an odd number");
+// }
